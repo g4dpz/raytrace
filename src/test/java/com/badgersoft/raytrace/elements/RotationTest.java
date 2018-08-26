18eef1f81048ed2087730d074f5ebd66d5133020
David Johnson <dave@badgersoft.com>
2018-08-26 16:20:31 +0100

    Rotation x implemented

--- src/test/java/com/badgersoft/raytrace/elements/RotationTest.java
+++ src/test/java/com/badgersoft/raytrace/elements/RotationTest.java
@@ -1,26 +1,30 @@
 package com.badgersoft.raytrace.elements;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class RotationTest {
 
-//    @Test
-//    public void xAxisRotation() {
-//        Point p1 = new Point(0, 1, 0);
-//        Matrix halfQuarter = Matrix.rotationX(Math.PI / 4.0);
-//        Matrix fullQuarter = Matrix.rotationX(Math.PI / 2.0);
-//        Point p2 = new Point(halfQuarter.multiply(p1));
-//        Point p3 = new Point(0.0, Math.sqrt(2.0) / 2.0, Math.sqrt(2.0) / 2.0);
-//        assertTrue(p3.equals(p2));
-//        Point p4 = new Point(fullQuarter.multiply(p1));
-//        Point p5 = new Point(0.0, 0.0, 1.0);
-//        assertTrue(p5.equals(p4));
-//    }
-//
-//    @Test
-//    public void inverseXAxisRotation() {
-//        Point p1 = new Point(0, 1, 0);
-//        Matrix halfQuarter = Matrix.rotationX(Math.PI / 4.0);
-//        Matrix inverse = Matrix.invert(halfQuarter);
-//        Point p2 = new Point(inverse.multiply(p1));
-//        assertTrue(new Point(0, Math.sqrt(2) / 2, Math.sqrt(2) / 2).equals(p2));
-//    }
+    @Test
+    public void xAxisRotation() {
+        Point p1 = new Point(0, 1, 0);
+        Matrix halfQuarter = Matrix.rotationX(Math.PI / 4.0);
+        Matrix fullQuarter = Matrix.rotationX(Math.PI / 2.0);
+        Point p2 = new Point(halfQuarter.multiply(p1));
+        Point p3 = new Point(0.0, Math.sqrt(2.0) / 2.0, Math.sqrt(2.0) / 2.0);
+        assertTrue(p3.equals(p2));
+        Point p4 = new Point(fullQuarter.multiply(p1));
+        Point p5 = new Point(0.0, 0.0, 1.0);
+        assertTrue(p5.equals(p4));
+    }
+
+    @Test
+    public void inverseXAxisRotation() {
+        Point p1 = new Point(0, 1, 0);
+        Matrix halfQuarter = Matrix.rotationX(Math.PI / 4.0);
+        Matrix inverse = Matrix.invert(halfQuarter);
+        Point p2 = new Point(inverse.multiply(p1));
+        assertTrue(new Point(0, Math.sqrt(2) / 2, -Math.sqrt(2) / 2).equals(p2));
+    }
 }
